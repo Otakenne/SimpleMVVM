31fd5cbf6c0ff1e337a4f4d0fd7ffbd138d93589
Otakenne <samuel.egbeo@gmail.com>
2021-10-21 01:36:36 +0100

    Project init

---
+++ app/src/main/java/com/celerii/simplemvvm/Activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.celerii.simplemvvm.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.celerii.simplemvvm.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/celerii/simplemvvm/Models/Holiday.java
@@ -0,0 +1,21 @@
+package com.celerii.simplemvvm.Models;
+
+public class Holiday {
+    String name, date;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
